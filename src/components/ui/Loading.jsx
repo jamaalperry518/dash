443ce77986cf9ae28b53791d13dc7dd0fcb27932
“jamaalperry518” <jamaalperry518@gmail.com>
2021-05-06 23:28:24 -0500

    added basic loading animation

--- src/components/ui/Loading.jsx
+++ src/components/ui/Loading.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { logo } from "../../assets/svg";
 import scss from "./loading.module.scss";
+import { motion } from "framer-motion";
 const Loading = () => {
-  return <div className={scss["loading-container"]}>{logo}</div>;
+  return (
+    <div className={scss["loading-container"]}>
+      <motion.div
+        animate={{
+          opacity: [0, 0.7, 1, 0.7, 0],
+          scale: [1.5, 1.8, 1.5, 1.6, 1.5],
+        }}
+      >
+        {logo}
+      </motion.div>
+    </div>
+  );
 };
 
 export default Loading;
