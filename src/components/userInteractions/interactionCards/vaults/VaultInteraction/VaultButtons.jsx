ec4ea64e02877d9ad960fff400764e73f8d718c0
“jamaalperry518” <jamaalperry518@gmail.com>
2021-06-07 17:14:09 -0500

    improved transition on opening vault interactions.

--- src/components/userInteractions/interactionCards/vaults/VaultInteraction/VaultButtons.jsx
+++ src/components/userInteractions/interactionCards/vaults/VaultInteraction/VaultButtons.jsx
@@ -3,6 +3,7 @@ import { convertStandardNumber } from "../../../../../Redux/actions/currencyActi
 import axios from "axios";
 import { FaAngleUp } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import { motion, AnimatePresence } from "framer-motion";
 //components
 import Withdraw from "./Withdraw";
 import Deposit from "./Deposit";
@@ -27,72 +28,95 @@ const VaultButtons = (props) => {
         console.log(err);
       });
   }, [vault.address]);
+
+  const variants = {
+    hidden: {
+      y: "-5rem",
+
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        default: {
+          type: "spring",
+          damping: 15,
+        },
+        duration: 0.3,
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: "-5rem",
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
   return (
     <IconContext.Provider value={{ className: "close-vault" }}>
-      <div
-        className={props.show ? "withdraw-deposit show" : "withdraw-deposit"}
-      >
-        <div className="balance-info">
-          <p className="section-heading">Your deposit in this vault</p>
-          <p className="user-deposit-in-vault">
-            {" "}
-            {vault.depositInVault} {vault.symbol}
-          </p>
-          <p className="deposit-fiat-amount">
-            ( ~ {convertStandardNumber(price * vault.depositInVault)})
-          </p>
-        </div>
-        <FaAngleUp
-          style={
-            {
-              // height: "3rem",
-              // width: "3rem",
-              // position: "absolute",
-              // cursor: "pointer",
-              // top: "8rem",
-              // left: "0",
-              // right: "0",
-              // margin: "0 auto",
-            }
-          }
-          onClick={props.vaultCloseHandler}
-          className="close-vault"
-        />
-        {(() => {
-          switch (cardDisplayed) {
-            case "deposit":
-              return <Deposit vault={props.vault} setCard={setCard} />;
-            case "withdraw":
-              return <Withdraw vault={props.vault} setCard={setCard} />;
-            case "stake":
-              return <Stake vault={props.vault} setCard={setCard} />;
+      <AnimatePresence>
+        {props.show && (
+          <motion.div
+            // initial={false}
+            initial={variants.hidden}
+            animate={variants.visible}
+            exit={variants.exit}
+            className="withdraw-deposit"
+          >
+            <div className="balance-info">
+              <p className="section-heading">Your deposit in this vault</p>
+              <p className="user-deposit-in-vault">
+                {" "}
+                {vault.depositInVault} {vault.symbol}
+              </p>
+              <p className="deposit-fiat-amount">
+                ( ~ {convertStandardNumber(price * vault.depositInVault)})
+              </p>
+            </div>
+            <FaAngleUp
+              onClick={props.vaultCloseHandler}
+              className="close-vault"
+            />
+            {(() => {
+              switch (cardDisplayed) {
+                case "deposit":
+                  return <Deposit vault={props.vault} setCard={setCard} />;
+                case "withdraw":
+                  return <Withdraw vault={props.vault} setCard={setCard} />;
+                case "stake":
+                  return <Stake vault={props.vault} setCard={setCard} />;
 
-            default:
-              return (
-                <div className="withdraw-deposit-buttons">
-                  <select
-                    onClick={(e) => setCard(e)}
-                    name="vault-actions"
-                    id="actions"
-                  >
-                    <option value="" className="action">
-                      Actions
-                    </option>
-                    <option value="deposit" className="action">
-                      Deposit
-                    </option>
-                    <option value="stake" className="action">
-                      Stake
-                    </option>
-                    <option value="withdraw" className="action">
-                      Withdraw
-                    </option>
-                  </select>
-                </div>
-              );
-          }
-        })()}
-      </div>
+                default:
+                  return (
+                    <div className="withdraw-deposit-buttons">
+                      <select
+                        onClick={(e) => setCard(e)}
+                        name="vault-actions"
+                        id="actions"
+                      >
+                        <option value="" className="action">
+                          Actions
+                        </option>
+                        <option value="deposit" className="action">
+                          Deposit
+                        </option>
+                        <option value="stake" className="action">
+                          Stake
+                        </option>
+                        <option value="withdraw" className="action">
+                          Withdraw
+                        </option>
+                      </select>
+                    </div>
+                  );
+              }
+            })()}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </IconContext.Provider>
   );
 };
